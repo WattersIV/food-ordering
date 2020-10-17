b0851bf73088bd8d906d07a57060e4aa600abab6
jesssubin <jess.s.moon@gmail.com>
2020-10-17 17:05:14 +0000

    Add helper function for food elements

---
+++ public/scripts/helperFunction.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+  loadFoods();
+})
+
+
+const createFoodElement = function(foodData) {
+  const $food = $(` <article class="food-container">
+           <img class="food-picture" src=""/>
+           <p class="food-name"></p>
+            <button type="button" class="add-menu-btn"> + </button>
+        </article>`);
+        return $food;
+};
+
+const renderFoods = function (foods) {
+  let $element;
+  const $container = $('#menu-section');
+  for (const food of foods) {
+    $element = createFoodElement(food);
+    $container.append($element);
+  }
+}
+
+const loadFoods = function () {
+  $.ajax('/order', { method: 'GET' })
+  .then(function(data) {
+    renderFoods(data);
+  });
+};
