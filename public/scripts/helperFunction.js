f43411de3ba8caf23cac3bfc67491cf0127402b7
jesssubin <jess.s.moon@gmail.com>
2020-10-17 17:18:47 +0000

    Add database.js with getAllFoods query function and amend helperFunction.js

--- public/scripts/helperFunction.js
+++ public/scripts/helperFunction.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
   loadFoods();
 })
 
-
 const createFoodElement = function(foodData) {
   const $food = $(` <article class="food-container">
-           <img class="food-picture" src=""/>
-           <p class="food-name"></p>
+           <img class="food-picture" src="${foods.food_picture_url}"/>
+           <p class="food-name">${foods.title}</p>
+           <p class="food-price">${foods.price_cents}</p>
             <button type="button" class="add-menu-btn"> + </button>
         </article>`);
         return $food;
