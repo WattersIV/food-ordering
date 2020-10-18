653598340a4a45560bb567b84eb846aedc0471a3
jesssubin <jess.s.moon@gmail.com>
2020-10-18 18:19:40 +0000

    fix errors

--- routes/order.js
+++ routes/order.js
@@ -1,25 +1,17 @@
 const express = require('express');
 const router  = express.Router();
 
-<<<<<<< HEAD
-// module.exports = (db) => {
-//   get compare cookie to DB and get info
-// };
-=======
 module.exports = (db) => {
-  router.get("/", (req, res, next) => {
-    return db.foods.all()
-    .then((data) => {
-      res.render("/order", { data });
-    })
-    .catch(err => {
-      res
-      .status(500)
-      .json({ error: err.message });
+  const getFoodItems = () => {
+    const queryString = `
+      SELECT * FROM foods;`
+      return db.query(queryString).then(res => res.rows)
+  };
+  router.get("/", (req, res) => {
+    getFoodItems()
+    .then(items => {
+      res.render("/order", { items })
     })
   })
   return router;
 };
->>>>>>> feature/order
-
-
