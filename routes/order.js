f4d3d527de357683af48e46768c4ab5a80d887d7
jesssubin <jess.s.moon@gmail.com>
2020-10-18 15:01:39 +0000

    Add order routes

---
+++ routes/order.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (db) => {
+  router.get("/", (req, res, next) => {
+    return db.foods.all()
+    .then((data) => {
+      res.render("/order", { data });
+    })
+    .catch(err => {
+      res
+      .status(500)
+      .json({ error: err.message });
+    })
+  })
+  return router;
+};
+
+
