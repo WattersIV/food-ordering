065b918ed6004ab37839e2157df44696e5f71c72
jesssubin <jess.s.moon@gmail.com>
2020-10-19 17:04:55 +0000

    Refactoring

--- public/scripts/foodList.js
+++ public/scripts/foodList.js
@@ -1,4 +1,3 @@
-const { getAllFoods } = require('../../server/database');
 
 $(document).ready(function(){
   loadFoods();
@@ -6,31 +5,32 @@ $(document).ready(function(){
 })
 
 const foodsData = getAllFoods();
+console.log(foodsData);
 
-const createFoodElement = function(foodsData) {
+// const createFoodElement = function(foodsData) {
 
-  const $food = $(` <article class="food-container">
-           <img class="food-picture" src="${foodsData.food_picture_url}"/>
-           <p class="food-name">${foodsData.title}</p>
-           <p class="food-price">${foodsData.price_cents}</p>
-            <button type="button" class="add-menu-btn"> + </button>
-        </article>`);
-        return $food;
-};
+//   const $food = $(` <article class="food-container">
+//            <img class="food-picture" src="${foodsData.food_picture_url}"/>
+//            <p class="food-name">${foodsData.title}</p>
+//            <p class="food-price">${foodsData.price_cents}</p>
+//             <button type="button" class="add-menu-btn"> + </button>
+//         </article>`);
+//         return $food;
+// };
 
-const renderFoods = function (foods) {
-  let $element;
-  const $container = $('#menu-section');
-  for (const food of foods) {
-    $element = createFoodElement(food);
-    $container.append($element);
-  }
-}
+// const renderFoods = function (foods) {
+//   let $element;
+//   const $container = $('#menu-section');
+//   for (const food of foods) {
+//     $element = createFoodElement(food);
+//     $container.append($element);
+//   }
+// }
 
-const loadFoods = function () {
-  $.ajax('/order', { method: 'GET' })
-  .then(renderFoods(foodsData));
-};
+// const loadFoods = function () {
+//   $.ajax('/order', { method: 'GET' })
+//   .then(renderFoods(foodsData));
+// };
 
 const addFoodToList = function (foodsData) {
   const $foodItem = $(` <ol>
@@ -48,15 +48,3 @@ const renderFoodToList = function () {
     })
 }
 
-
-// function for query to edit menu
-
-// function to delete query item
-
-// function to change unconfirmed order to order
-
-// function for listing out the menu names on the menu editor
-
-// function for calculating total cost
-
-// function for radio boxes- order-ready or order-not ready
--- server/database.js
+++ server/database.js
@@ -1,38 +1,28 @@
 module.exports = db => {
 
-const getAllFoods = () => {
-  const queryString =
-  `SELECT foods.*
-  FROM foods
-  GROUP BY type`
-  return pool.query(queryString)
-  .then(response => response.rows);
-};
+  const getAllFoods = () => {
+    const queryString =
+    `SELECT foods.*
+    FROM foods
+    GROUP BY type`
+    return pool.query(queryString)
+    .then(response => response.rows);
+  };
 
-// function to list unconfirmed orders
-const getUnconfOrders = () => {
-  const queryString = `
-  SELECT id FROM orders
-  WHERE order_completed = false;
-  `;
-  return db.query(queryString)
-  .then(response => response.rows)
-};
+  // function to list unconfirmed orders
+  const getUnconfOrders = () => {
+    const queryString = `
+    SELECT id FROM orders
+    WHERE order_completed = false;
+    `;
+    return pool.query(queryString)
+    .then(response => response.rows)
+  };
 
-const getFoodItemsByName = () => {
-  const queryString = `
-  SELECT title FROM foods;
-  `;
-  return db.query(queryString)
-  .then (response => response.rows);
-};
-
-return {
-  getAllFoods,
-  getUnconfOrders,
-  getFoodItemsByName
-}
-
+  return {
+    getAllFoods,
+    getUnconfOrders
+  }
 }
 
 
