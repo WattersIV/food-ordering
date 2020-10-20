0241ed15895abc0a3ebc9bb9ba5622d71ac391c9
jesssubin <jess.s.moon@gmail.com>
2020-10-20 22:42:15 +0000

    Decorating order page in progress

--- db/seeds/02_foods.sql
+++ db/seeds/02_foods.sql
@@ -4,7 +4,7 @@ INSERT INTO users (name, email, phone, address, postal_code) VALUES
 ('Daniel', 'dan@dan.com', '555-1234', '456 Dan Street', 'H3X2E6');
 
 
-INSERT INTO foods (food_picture_url, title, price_cents, type) VALUES ('https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.thedailymeal.com%2Feat%2Fbest-pasta-dish-every-state&psig=AOvVaw188iznKpiPBlvxu4IVnVbO&ust=1603034658414000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPjfyc73u-wCFQAAAAAdAAAAABAD', 'Spaghetti Bolonese', 1499, 'Main'),
-('https://thumbor.thedailymeal.com/WZGzKREo6uhaR9SaEdlw8LR4tV8=/870x565/https://www.thedailymeal.com/sites/default/files/2019/04/01/bestpasta-main-istock_crop.jpg', 'Frutti di Mare', 1699, 'Main'),
-('https://www.skinnytaste.com/wp-content/uploads/2019/10/Spaghetti-Carbonara-6.jpg', 'Carbonara', 1599, 'Main');
+INSERT INTO foods (food_picture_url, title, price_cents, type) VALUES ('https://images.pexels.com/photos/1055271/pexels-photo-1055271.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 'Vanilla Cupcake', 499, 'Cupcake'),
+('https://images.pexels.com/photos/1055272/pexels-photo-1055272.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 'Chocolate cupcake', 499, 'Cupcake'),
+('https://images.pexels.com/photos/1055270/pexels-photo-1055270.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 'Fruit cupcake', 499, 'Cupcake');
 
