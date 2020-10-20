b5541b7ba82672e2c6cc040f23777fd3a1db4918
Bill Watters <billywatters13@gmail.com>
2020-10-20 20:34:45 +0000

    Quantity increments but for the Carbonara only

--- public/scripts/foodList.js
+++ public/scripts/foodList.js
@@ -12,7 +12,7 @@ const addFoodElement = (name, price) => {
     <td id="food-quantity">
       <form  id='${name}' method='POST' action='#'>
         <input type='button' value='-' id='${name}-minus' class='qtyminus' field='quantity' />
-        <input type='text' name='quantity' value='0' class='qty' />
+        <input type='text' name='${name}-quantity' value='0' class='qty' />
         <input type='button' value='+' id='${name}-plus' class='qtyplus' field='quantity' />
       </form>
     </td>
@@ -28,26 +28,26 @@ const appendFoodToList = () => {
   const container = $("#order-container");
   $(".add-menu-btn").click(function (evt) {
     container.append(addFoodElement(evt.target.parentElement.children[1].textContent, evt.target.parentElement.children[2].textContent))
-    nam = $(this).attr('name')
+    console.log($(this).attr('name'))
+    nam = $(this).attr('name');
   });
-    console.log(nam)
 
     container.on("click", `.qtyplus`, e => {
-      console.log(nam)
-      const fieldName = $(`${nam}-plus`).attr('id');
-      let currentVal = parseInt($(`input[name=${fieldName}]`).val());
+      const fieldName = `${nam}-quantity`;
+      let currentVal = parseInt($(`input[name=${nam}-quantity]`).val());
       console.log(fieldName, currentVal)
       if (!isNaN(currentVal)) {
+        console.log('if')
           $(`input[name=${fieldName}]`).val(currentVal + 1);
       } else {
-          $(`input[name=${fieldName}]`).val(0);
+        console.log('else')
+          $(`input[name=${nam}-quantity]`).val(0);
       }
     });
 
 
      container.on("click", `.qtyminus`, e => {
-      console.log($(`${nam}-minus`))
-      const fieldName = $(`${nam}-minus`).attr('field');
+      const fieldName = `${nam}-quantity`;
       let currentVal = parseInt($(`input[name=${fieldName}]`).val());
       console.log(fieldName, currentVal)
       if (!isNaN(currentVal) && currentVal > 0) {
