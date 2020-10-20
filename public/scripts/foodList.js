3d701eae15f2010381c95ba7f209698f31c4939d
Bill Watters <billywatters13@gmail.com>
2020-10-20 22:40:42 +0000

    Add functional quanitity buttons in the order page

--- public/scripts/foodList.js
+++ public/scripts/foodList.js
@@ -5,20 +5,21 @@ $(document).ready(function(){
 const foodItems = {};
 
 const addFoodElement = (name, price) => {
+  const newName = name.replace(/\s/g, '')
   const foodItem = `
   <tr>
-    <td id="food-name">${name}</td>
+    <td id="food-name">${newName}</td>
     <td id="food-price">${price}</td>
     <td id="food-quantity">
-      <form  id='${name}' method='POST' action='#'>
-        <input type='button' value='-' id='${name}-minus' class='qtyminus' field='quantity' />
-        <input type='text' name='${name}-quantity' value='0' class='qty' />
-        <input type='button' value='+' id='${name}-plus' class='qtyplus' field='quantity' />
+      <form  id='${newName}' method='POST' action='#'>
+        <input type='button' value='-' id='${newName}-minus' class='qtyminus' field='quantity' />
+        <input type='text' name='${newName}-quantity' value='0' class='qty' />
+        <input type='button' value='+' id='${newName}-plus' class='qtyplus' field='quantity' />
       </form>
     </td>
   </tr>
   `;
-  console.log('LOOK HERE!',name)
+  console.log('LOOK HERE!',newName)
   foodItems
   return foodItem;
 }
@@ -29,31 +30,31 @@ const appendFoodToList = () => {
   $(".add-menu-btn").click(function (evt) {
     container.append(addFoodElement(evt.target.parentElement.children[1].textContent, evt.target.parentElement.children[2].textContent))
     console.log($(this).attr('name'))
-    nam = $(this).attr('name');
+    nam = $(this).attr('name').replace(/\s/g, '')
   });
 
     container.on("click", `.qtyplus`, e => {
-      const fieldName = `${nam}-quantity`;
-      let currentVal = parseInt($(`input[name=${nam}-quantity]`).val());
+
+      const fieldName = $(e.target).siblings("input.qty")
+      let currentVal = parseInt(fieldName.val());
       console.log(fieldName, currentVal)
       if (!isNaN(currentVal)) {
-        console.log('if')
-          $(`input[name=${fieldName}]`).val(currentVal + 1);
+          fieldName.val(currentVal + 1);
       } else {
         console.log('else')
-          $(`input[name=${nam}-quantity]`).val(0);
+          fieldName.val(0);
       }
     });
 
 
      container.on("click", `.qtyminus`, e => {
-      const fieldName = `${nam}-quantity`;
-      let currentVal = parseInt($(`input[name=${fieldName}]`).val());
+      const fieldName = $(e.target).siblings("input.qty")
+      let currentVal = parseInt(fieldName.val());
       console.log(fieldName, currentVal)
       if (!isNaN(currentVal) && currentVal > 0) {
-          $(`input[name=${fieldName}]`).val(currentVal - 1);
+          fieldName.val(currentVal - 1);
       } else {
-          $(`input[name=${fieldName}]`).val(0);
+          fieldName.val(0);
       }
   });
 
