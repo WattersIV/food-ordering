7c4d57e71e8e7738faa8348dc9ea4e0f37a020b5
danschae <danschae@gmail.com>
2020-10-20 20:34:28 +0000

    edit phone number

--- helpers/sms_helpers.js
+++ helpers/sms_helpers.js
@@ -6,7 +6,18 @@ const authToken = process.env.authToken;
 const from = process.env.fromNumber;
 
 const client = require('twilio')(accountSid, authToken);
-return client.messages.create({body, from, to})};
+return client.messages.create({body, from, to})
+
+};
+
+const sendMessageToAdmin = (order) => {
+  let message = `
+  Restaurant Name:
+
+  We've just received an order for: \n`;
+
+
+}
 
 
 sendMessage("hello world", "4388664160").then(response => console.log(response.body));
