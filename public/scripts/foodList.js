3051eb9d19d13a9e39261e01fbdd9646ef71bac2
Bill Watters <billywatters13@gmail.com>
2020-10-20 17:00:20 +0000

    Counter counts by 1s instead of 1* num of items

--- public/scripts/foodList.js
+++ public/scripts/foodList.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
  appendFoodToList();
 });
 
+const foodItems = [];
 
 const addFoodElement = (name, price) => {
   const foodItem = `
@@ -9,14 +10,15 @@ const addFoodElement = (name, price) => {
     <td id="food-name">${name}</td>
     <td id="food-price">${price}</td>
     <td id="food-quantity">
-    <form id='myform' method='POST' action='#'>
-    <input type='button' value='-' class='qtyminus' field='quantity' />
-    <input type='text' name='quantity' value='0' class='qty' />
-    <input type='button' value='+' class='qtyplus' field='quantity' />
-</form>
+      <form id='myform' method='POST' action='#'>
+        <input type='button' value='-' class='qtyminus' field='quantity' />
+        <input id='${name}' type='text' name='quantity' value='0' class='qty' />
+        <input type='button' value='+' class='qtyplus' field='quantity' />
+      </form>
     </td>
   </tr>
   `;
+  foodItems.push(name);
   return foodItem;
 }
 
@@ -28,6 +30,7 @@ const appendFoodToList = () => {
       event.preventDefault();
       fieldName = $(this).attr('field');
       let currentVal = parseInt($(`input[name=${fieldName}]`).val());
+      console.log(currentVal)
       if (!isNaN(currentVal)) {
           $(`input[name=${fieldName}]`).val(currentVal + 1);
       } else {
@@ -38,6 +41,7 @@ const appendFoodToList = () => {
       event.preventDefault();
       fieldName = $(this).attr('field');
       let currentVal = parseInt($(`input[name=${fieldName}]`).val());
+      console.log(currentVal)
       if (!isNaN(currentVal) && currentVal > 0) {
           $(`input[name=${fieldName}]`).val(currentVal - 1);
       } else {
