06fd6e5c3d1dce6afa437a211712bfc434d16929
Bill Watters <billywatters13@gmail.com>
2020-10-20 17:44:07 +0000

    Counter counts by 1s instead of 1* num of items last commit did not solve it

--- public/scripts/foodList.js
+++ public/scripts/foodList.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
  appendFoodToList();
 });
 
-const foodItems = [];
+const foodItems = {};
 
 const addFoodElement = (name, price) => {
   const foodItem = `
@@ -10,15 +10,16 @@ const addFoodElement = (name, price) => {
     <td id="food-name">${name}</td>
     <td id="food-price">${price}</td>
     <td id="food-quantity">
-      <form id='myform' method='POST' action='#'>
-        <input type='button' value='-' class='qtyminus' field='quantity' />
-        <input id='${name}' type='text' name='quantity' value='0' class='qty' />
-        <input type='button' value='+' class='qtyplus' field='quantity' />
+      <form  id='${name}' method='POST' action='#'>
+        <input type='button' value='-' id='${name}-minus' class='qtyminus' field='quantity' />
+        <input type='text' name='quantity' value='0' class='qty' />
+        <input type='button' value='+' id='${name}-plus' class='qtyplus' field='quantity' />
       </form>
     </td>
   </tr>
   `;
-  foodItems.push(name);
+  console.log('LOOK HERE!',name)
+  foodItems
   return foodItem;
 }
 
@@ -26,7 +27,10 @@ const appendFoodToList = () => {
   const container = $("#order-container");
   $(".add-menu-btn").click(function (evt) {
     container.append(addFoodElement(evt.target.parentElement.children[1].textContent, evt.target.parentElement.children[2].textContent))
-    $('.qtyplus').click(function(event){
+    const name = $(this).attr('name')//.parent().attr('id')
+    console.log(name)
+
+    $(`#${name}-plus.qtyplus`).click(function(event){
       event.preventDefault();
       fieldName = $(this).attr('field');
       let currentVal = parseInt($(`input[name=${fieldName}]`).val());
@@ -37,7 +41,7 @@ const appendFoodToList = () => {
           $(`input[name=${fieldName}]`).val(0);
       }
   });
-  $(".qtyminus").click(function(event) {
+  $(`#${name}-minus.qtyminus`).click(function(event) {
       event.preventDefault();
       fieldName = $(this).attr('field');
       let currentVal = parseInt($(`input[name=${fieldName}]`).val());
