8399d3d94b93623e3531c34cb74049501a58c7db
Bill Watters <billywatters13@gmail.com>
2020-10-21 21:44:20 +0000

    object not being sent to line 55

--- routes/order.js
+++ routes/order.js
@@ -16,8 +16,7 @@ module.exports = (db) => {
   });
 
   router.post("/:id/confirm", (req, res) => {
-
-    console.log(Object.keys(req.body))
+    const keys = Object.keys(req.body)
     const order_id = req.session.cart.cart_id
     const queryString = `
     UPDATE orders
@@ -26,25 +25,49 @@ module.exports = (db) => {
     RETURNING *;
     `;
     db.query(queryString)
-    //    .then(() => {
-    //   db.query(`
-    //   SELECT *
-    //   FROM foods
-    //   WHERE name LIKE "%${}%"    `)
-    //  })
+      .then(() => {
+        let foods = {}
+        console.log(keys)
+
+        for (let i = 0; i < keys.length; i++) {
+          let sql =`
+          SELECT id
+          FROM foods
+          `
+          let str= ''
+          str += `WHERE title='${keys[i]}' ;`
+          sql+= str;
+          db.query(sql)
+            .then ((data) => {
+            //console.log(data.rows[0].id, req.body[name])
+              foods[keys[i]] = {title: keys[i],
+                                id: data.rows[0].id,
+                                quantity: Number(req.body[keys[i]])}
+              if (i === keys.length - 1){
+                console.log('LINE 46', foods) //OBJ
+                return foods;
+              }
+            })
+        }
+        //console.log('ORDER HERE', foods)
+        return foods
+      })
+      .then((data) => console.log('LINE 55', data)) //UNDEF
+
+
     // .then(() => {
     //   db.query(`
     //   INSERT INTO food_carts (order_id, food_id, quantity)
     //   VALUES()`)
     // })
-    .then(() => {
-      console.log('HERE!')
-      res.render("thank-you", {data: req.session})
-    })
-  })
+  //   .then((result) => {
+  //     console.log('000000000000000',result)
+  //     res.render("thank-you", {data: req.session})
+  //   })
+  //   .catch((err) => console.log(err))
+   })
 
 
 
   return router;
-
-};
+  };
