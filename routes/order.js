7fa9d1d38c76ba5e28ea1ecb0d11a88ee7a35c9a
danschae <danschae@gmail.com>
2020-10-21 02:46:50 +0000

    refactor order routes

--- routes/order.js
+++ routes/order.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const router  = express.Router();
 
-module.exports = (db) => {
+module.exports = ({getFoodItems, confirmOrder}) => {
 
-  const getFoodItems = () => {
-    const queryString = `SELECT * FROM foods;`;
-      return db.query(queryString).then(response => response.rows);
-  };
 
   router.get("/:id", (req, res) => {
     getFoodItems()
@@ -15,15 +11,12 @@ module.exports = (db) => {
     })
   });
 
+  // change to a function
+
   router.post("/:id/confirm", (req, res) => {
     const order_id = req.session.cart.cart_id
-    const queryString = `
-    UPDATE orders
-    SET order_processed = TRUE
-    where id = ${order_id}
-    RETURNING *;
-    `;
-    db.query(queryString)
+
+    confirmOrder(order_id)
     .then(() => {
       console.log('HERE!')
       res.render("thank-you", {data: req.session})
--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
 app.use('/', mainRoutes(db));
 app.use("/admin", adminRoutes(databaseHelpers));
-app.use("/order", orderRoutes(db));
+app.use("/order", orderRoutes(databaseHelpers));
 
 // Home page
 // Warning: avoid creating more routes in this file!
