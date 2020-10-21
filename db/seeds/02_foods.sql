9751d5095eebc40fe6aa873d711dbe7de7d02b49
jesssubin <jess.s.moon@gmail.com>
2020-10-21 02:24:34 +0000

    new seeds now in, styling almost done

--- db/seeds/02_foods.sql
+++ db/seeds/02_foods.sql
@@ -6,5 +6,16 @@ INSERT INTO users (name, email, phone, address, postal_code) VALUES
 
 INSERT INTO foods (food_picture_url, title, price_cents, type) VALUES ('https://images.pexels.com/photos/1055271/pexels-photo-1055271.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 'Vanilla Cupcake', 499, 'Cupcake'),
 ('https://images.pexels.com/photos/1055272/pexels-photo-1055272.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 'Chocolate cupcake', 499, 'Cupcake'),
-('https://images.pexels.com/photos/1055270/pexels-photo-1055270.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 'Fruit cupcake', 499, 'Cupcake');
-
+('https://images.pexels.com/photos/1055270/pexels-photo-1055270.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 'Fruit cupcake', 499, 'Cupcake'),
+('http://bobbetteandbelle.com/images/1f_wedcup/deccup3.jpg', 'Hydrangea Cupcake', 599, 'Cupcake'),
+('http://bobbetteandbelle.com/images/1f_wedcup/deccup8.jpg', 'Petunia Cupcake', 599, 'Cupcake'),
+('http://bobbetteandbelle.com/images/1f_wedcup/deccup4.jpg', 'Peony Cupcake', 599, 'Cupcake'),
+('http://bobbetteandbelle.com/images/2_macaron/mac4.jpg', 'Rasberry Macaron', 249, 'Macaron'),
+('http://bobbetteandbelle.com/images/2_macaron/mac11.jpg', 'Earl Grey Macaron', 249, 'Macaron'),
+('http://bobbetteandbelle.com/images/2_macaron/mac1.jpg', 'Salted Caramel Macaron', 249, 'Macaron'),
+('http://bobbetteandbelle.com/images/2_macaron/mac8.jpg', 'Pistachio Macaron', 249, 'Macaron'),
+('http://bobbetteandbelle.com/images/2_macaron/mac3.jpg', 'Milk Choco Caramel Macaron', 249, 'Macaron'),
+('http://bobbetteandbelle.com/images/2_macaron/mac5.jpg', 'Chocolate Macaron', 249, 'Macaron'),
+('http://bobbetteandbelle.com/images/2_macaron/mac2.jpg', 'Black Current Macaron', 249, 'Macaron'),
+('http://bobbetteandbelle.com/images/2_macaron/mac9.jpg', 'Hazelnut Macaron', 249, 'Macaron'),
+('http://bobbetteandbelle.com/images/2_macaron/mac6.jpg', 'Vanilla Macaron', 249, 'Macaron');
