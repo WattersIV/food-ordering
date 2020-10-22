2cd8f391be47da9fbb7f86caa1543d8a753c0adb
jesssubin <jess.s.moon@gmail.com>
2020-10-22 17:31:53 +0000

    implementing bootstrap for header/footer

--- public/scripts/foodList.js
+++ public/scripts/foodList.js
@@ -17,7 +17,7 @@ const addFoodElement = (name, price) => {
     <form id='myform' method='POST' action='#'>
     <div class='qty-div'>
     <input id='minus-btn' type='button' value='-' class='qtyminus' field='quantity' />
-    <input type='text' name='quantity' value='0' class='qty' />
+    <input type='text' name='quantity' value='1' class='qty' />
     <input id='plus-btn' type='button' value='+' class='qtyplus' field='quantity' />
     </div>
 </form>
